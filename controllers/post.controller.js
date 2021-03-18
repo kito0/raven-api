9b853d492b2fdd76b5c1272c25e9666c18e70a11
kito0 <drewshah0@gmail.com>
2021-03-18 01:08:18 -0600

    removed image import

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,11 +1,10 @@
-//import imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+const Post = require('../models/post.model');
 
-exports.getData = async (req, res) => {
+exports.getPosts = async (req, res) => {
 	res.send(req.user);
 };
 
 exports.post = async (req, res) => {
-	const fileName = req.file != null ? req.file.filename : null;
 	const post = new Post({
 		title: req.body.title,
 		body: req.body.body,
@@ -16,12 +15,3 @@ exports.post = async (req, res) => {
 		const newPost = post.save();
 	} catch {}
 };
-
-function saveImage(image, coverEncoded) {
-	if (coverEncoded == null) return;
-	const cover = JSON.parse(coverEncoded);
-	if (cover != null && imageMimeTypes.includes(cover.type)) {
-		image.coverImage = new Buffer.from(cover.data, 'base64');
-		image.coverImageType = cover.type;
-	}
-}
