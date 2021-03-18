7ec6d7a3f5806f57fce5f8b4502004407636688a
kito0 <drewshah0@gmail.com>
2021-03-17 23:34:12 -0600

    created filler component for later

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import './css/App.css';
 import Sidebar from './components/sidebar/Sidebar';
+import Feed from './components/feed/Feed';
+import Widgets from './components/widgets/Widgets';
 
 function App() {
 	return (
 		<div className="app">
 			<Sidebar />
+			<Feed />
+			<Widgets />
 		</div>
 	);
 }
---
+++ client/src/components/widgets/Widgets.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './css/widgets.css';
+
+export default function widgets() {
+	return <div>widgets</div>;
+}
