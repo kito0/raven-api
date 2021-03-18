806376a4c552dbe5d41a44365fc8ecef4d25fc41
kito0 <drewshah0@gmail.com>
2021-03-18 01:09:21 -0600

    added handle validation

--- util/validation.js
+++ util/validation.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const registerValidation = (data) => {
 	const schema = Joi.object({
 		name: Joi.string().min(4).required(),
+		handle: Joi.string().min(4).required(),
 		email: Joi.string().min(6).required().email(),
 		password: Joi.string().min(6).required(),
 	});
