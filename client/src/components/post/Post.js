95236fe0a34584c799eaaca13fd1f42d3836ef16
kito0 <drewshah0@gmail.com>
2021-03-18 08:54:13 -0600

    updated elements to receive props instead of placeholders

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Avatar, Typography } from '@material-ui/core';
+import { Avatar, IconButton, Typography } from '@material-ui/core';
 import {
 	ChatBubbleOutline,
 	FavoriteBorder,
@@ -12,28 +12,36 @@ export default function Post({ name, handle, avatar, text, image, timestamp }) {
 	return (
 		<div className="post">
 			<div className="post__avatar">
-				<Avatar src={process.env.PUBLIC_URL + 'avatar.jpg'} />
+				<Avatar src={{ avatar }} />
 			</div>
 			<div className="post__body">
 				<div className="post__header">
 					<div className="post__header__text">
 						<Typography variant="h6" className="post__header__name">
-							Andrew Shah
+							{name}
 						</Typography>
 						<Typography variant="caption" className="post__header__handle">
-							@andrewshah
+							{handle}
 						</Typography>
 					</div>
 					<div className="post__header__description">
-						<Typography variant="body2">test description</Typography>
+						<Typography variant="body2">{text}</Typography>
 					</div>
 				</div>
-				<img src={image} alt="" />
+				<img src={image} alt="" className="post__img" />
 				<div className="post__footer">
-					<ChatBubbleOutline fontSize="small" />
-					<Repeat fontSize="small" />
-					<FavoriteBorder fontSize="small" />
-					<Publish fontSize="small" />
+					<IconButton>
+						<ChatBubbleOutline fontSize="small" />
+					</IconButton>
+					<IconButton>
+						<Repeat fontSize="small" />
+					</IconButton>
+					<IconButton>
+						<FavoriteBorder fontSize="small" />
+					</IconButton>
+					<IconButton>
+						<Publish fontSize="small" />
+					</IconButton>
 				</div>
 			</div>
 		</div>
