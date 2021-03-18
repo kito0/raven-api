d0e48463452fcdf60d7318b8daefbb5ed54ea276
kito0 <drewshah0@gmail.com>
2021-03-18 00:45:26 -0600

    added search skeleton

--- client/src/components/widgets/Widgets.js
+++ client/src/components/widgets/Widgets.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './css/widgets.css';
+import { Search } from '@material-ui/icons';
 
 export default function widgets() {
-	return <div>widgets</div>;
+	return (
+		<div className="widgets">
+			<div className="widgets__input">
+				<Search className="widget__search" />
+				<input placeholder="Search Raven" type="text" />
+			</div>
+		</div>
+	);
 }
