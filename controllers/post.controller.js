c05565d0931efa9658479a3eea121a3370d8f9c6
kito0 <drewshah0@gmail.com>
2021-03-20 06:09:04 -0600

    added edit and delete endpoints for post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,17 +1,37 @@
 const Post = require('../models/post.model');
 
+// GET http://localhost:5000/api/post
 exports.getPosts = async (req, res) => {
-	res.send(req.user);
+	Post.find()
+		.then((posts) => {
+			res.status(200).json(posts);
+		})
+		.catch((err) => res.status(500).send(err));
 };
 
-exports.post = async (req, res) => {
-	const post = new Post({
-		title: req.body.title,
-		body: req.body.body,
-	});
-	saveImage(image, req.body.cover);
+// POST http://localhost:5000/api/post
+exports.createPost = async (req, res) => {
+	Post.create(req.body)
+		.then((post) => {
+			res.status(201).json(post);
+		})
+		.catch((err) => res.status(500).send(err));
+};
 
-	try {
-		const newPost = post.save();
-	} catch {}
+// PUT http://localhost:5000/api/post/:id
+exports.editPost = async (req, res) => {
+	Post.findByIdAndUpdate(req.params.id, req.body)
+		.then(() => {
+			res.status(200).send('post edited');
+		})
+		.catch((err) => res.status(400).send(err));
+};
+
+// DELETE http://localhost:5000/api/post/:id
+exports.deletePost = async (req, res) => {
+	Post.findByIdAndDelete(req.params.id)
+		.then(() => {
+			res.status(200).send('post deleted');
+		})
+		.catch((err) => res.status(400).send(err));
 };
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
-const { getPosts, post } = require('../controllers/post.controller');
+const {
+	getPosts,
+	createPost,
+	editPost,
+	deletePost,
+} = require('../controllers/post.controller');
 const verify = require('../util/verify.token');
 
 router.get('/', getPosts);
 
-router.post('/', verify, post);
+router.post('/', verify, createPost);
+
+router.put('/:id', verify, editPost);
+
+router.delete('/:id', verify, deletePost);
 
 module.exports = router;
