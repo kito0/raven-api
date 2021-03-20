5d3f787e3fc0203531c94c5cf3b3a80b515e0e8e
kito0 <drewshah0@gmail.com>
2021-03-20 06:41:19 -0600

    added axios posts fetch

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,69 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './css/feed.css';
+import axios from 'axios';
 import CroakBox from './CroakBox';
 import Post from '../post/Post';
 
 export default function Feed() {
+	const [posts, setPosts] = useState([]);
+	const api = 'http://localhost:5000/api/posts';
+
+	useEffect(() => {
+		axios
+			.get(api)
+			.then((res) => setPosts(res.data))
+			.catch((err) => console.error(err));
+	}, [posts]);
+
 	return (
 		<div className="feed">
 			<div className="feed__header">Raven</div>
 			<CroakBox />
-			<Post
-				name="Andrew Shah"
-				handle="@andrewshah"
-				avatar="https://s2.wp.com/wp-content/themes/premium/collective/images/default-profile.jpg"
-				text="sample text"
-				image="https://i.pinimg.com/originals/64/46/c3/6446c3c9adf0ed6cf52f4ba61d13f3e6.gif"
-			/>
-			<Post
-				name="Andrew Shah"
-				handle="@andrewshah"
-				avatar="https://s2.wp.com/wp-content/themes/premium/collective/images/default-profile.jpg"
-				text="sample text"
-				image="https://cdn.vox-cdn.com/thumbor/EaUuzIdnUGXAs_LokdLgtdrJZCY=/0x0:420x314/1400x1050/filters:focal(136x115:202x181):format(gif)/cdn.vox-cdn.com/uploads/chorus_image/image/55279403/tenor.0.gif"
-			/>
-			<Post
-				name="Andrew Shah"
-				handle="@andrewshah"
-				avatar="https://s2.wp.com/wp-content/themes/premium/collective/images/default-profile.jpg"
-				text="sample text"
-				image="https://cdn2.scratch.mit.edu/get_image/gallery/723870_200x130.png?v=1417640727.0"
-			/>
-			<Post
-				name="Andrew Shah"
-				handle="@andrewshah"
-				avatar="https://s2.wp.com/wp-content/themes/premium/collective/images/default-profile.jpg"
-				text="sample text"
-				image="https://eanbowman.com/gif/jake_check_it_dude_100_percent_awesome_atude.gif"
-			/>
-			<Post
-				name="Andrew Shah"
-				handle="@andrewshah"
-				avatar="https://s2.wp.com/wp-content/themes/premium/collective/images/default-profile.jpg"
-				text="sample text"
-				image="https://i.pinimg.com/originals/64/46/c3/6446c3c9adf0ed6cf52f4ba61d13f3e6.gif"
-			/>
-			<Post
-				name="Andrew Shah"
-				handle="@andrewshah"
-				avatar="https://s2.wp.com/wp-content/themes/premium/collective/images/default-profile.jpg"
-				text="sample text"
-				image="https://cdn.vox-cdn.com/thumbor/EaUuzIdnUGXAs_LokdLgtdrJZCY=/0x0:420x314/1400x1050/filters:focal(136x115:202x181):format(gif)/cdn.vox-cdn.com/uploads/chorus_image/image/55279403/tenor.0.gif"
-			/>
-			<Post
-				name="Andrew Shah"
-				handle="@andrewshah"
-				avatar="https://s2.wp.com/wp-content/themes/premium/collective/images/default-profile.jpg"
-				text="sample text"
-				image="https://cdn2.scratch.mit.edu/get_image/gallery/723870_200x130.png?v=1417640727.0"
-			/>
-			<Post
-				name="Andrew Shah"
-				handle="@andrewshah"
-				avatar="https://s2.wp.com/wp-content/themes/premium/collective/images/default-profile.jpg"
-				text="sample text"
-				image="https://eanbowman.com/gif/jake_check_it_dude_100_percent_awesome_atude.gif"
-			/>
+			{posts ? (
+				posts
+					.slice(0)
+					.reverse()
+					.map((post) => (
+						<Post
+							name={post.name}
+							handle={post.handle}
+							text={post.text}
+							image={post.image}
+						/>
+					))
+			) : (
+				<p>no posts available</p>
+			)}
 		</div>
 	);
 }
