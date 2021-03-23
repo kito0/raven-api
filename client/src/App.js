e1ebe2cddbdbe9d53140efb71c0e17cd892c3d1c
kito0 <drewshah0@gmail.com>
2021-03-23 00:11:24 -0600

    added user auth states for route redirects

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import './css/App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Feed from './components/feed/Feed';
@@ -6,13 +12,20 @@ import Widgets from './components/widgets/Widgets';
 import Login from './components/login/Login';
 
 function App() {
+	const user = useSelector((state) => state.userSlice.user);
+	const authenticated = useSelector((state) => state.userSlice.authenticated);
+
 	return (
 		<Router>
 			<div className="app">
 				<Sidebar />
 				<Switch>
-					<Route exact path="/" component={Feed} />
-					<Route exact path="/login" component={Login} />
+					<Route exact path="/">
+						<Feed />
+					</Route>
+					<Route exact path="/login">
+						{authenticated ? <Redirect to="/" /> : <Login />}
+					</Route>
 				</Switch>
 				<Widgets />
 			</div>
