c7946f3fad347da1dc01f9c593bab8e415c506c0
kito0 <drewshah0@gmail.com>
2021-03-22 19:13:00 -0600

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './css/App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Feed from './components/feed/Feed';
 import Widgets from './components/widgets/Widgets';
+import Login from './components/login/Login';
 
 function App() {
 	return (
-		<div className="app">
-			<Sidebar />
-			<Feed />
-			<Widgets />
-		</div>
+		<Router>
+			<div className="app">
+				<Sidebar />
+				<Switch>
+					<Route exact path="/" component={Feed} />
+					<Route exact path="/login" component={Login} />
+				</Switch>
+				<Widgets />
+			</div>
+		</Router>
 	);
 }
 
