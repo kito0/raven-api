8a226b16b6f20fae3151f7e1963b4a3f1a74bf4d
kito0 <drewshah0@gmail.com>
2021-03-24 01:01:05 -0600

    added avatar uri validation

--- util/validation.js
+++ util/validation.js
@@ -5,6 +5,7 @@ const registerValidation = (data) => {
 		name: Joi.string().min(4).required(),
 		handle: Joi.string().min(4).required(),
 		email: Joi.string().min(6).required().email(),
+		avatar: Joi.string().min(6).required().uri(),
 		password: Joi.string().min(6).required(),
 	});
 	return schema.validate(data);
