96daf61263feecc8373f8115d22e3ea4f5ea68ab
kito0 <drewshah0@gmail.com>
2021-03-24 00:56:42 -0600

    added authentication to access posting

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './css/feed.css';
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 import CroakBox from './CroakBox';
 import Post from '../post/Post';
 
 export default function Feed() {
 	const [posts, setPosts] = useState([]);
+	const dispatch = useDispatch();
+	const user = useSelector((state) => state.userSlice.user);
+	const authenticated = useSelector((state) => state.userSlice.authenticated);
+
 	const api = 'https://raven-x.herokuapp.com/api/posts';
+	//const api = 'http://localhost:5000/api/posts';
 
 	useEffect(() => {
 		axios
@@ -18,7 +24,8 @@ export default function Feed() {
 	return (
 		<div className="feed">
 			<div className="feed__header">Raven</div>
-			<CroakBox />
+			{authenticated && <CroakBox />}
+
 			{posts ? (
 				posts
 					.slice(0)
