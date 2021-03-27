c87e67053cfe87964688f404bd947ef72825fb06
kito0 <drewshah0@gmail.com>
2021-03-27 00:20:38 -0600

    removed unused states

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './css/feed.css';
-//import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import CroakBox from './CroakBox';
 import Post from '../post/Post';
@@ -8,24 +7,14 @@ import { GetPosts } from '../../redux/posts';
 
 export default function Feed() {
 	const dispatch = useDispatch();
-	const user = useSelector((state) => state.userSlice.user);
+
 	const posts = useSelector((state) => state.postsSlice.posts);
 	const authenticated = useSelector((state) => state.userSlice.authenticated);
 	const loading = useSelector((state) => state.postsSlice.loading);
 
-	const api = 'https://raven-x.herokuapp.com/api/posts';
-	//const api = 'http://localhost:5000/api/posts';
-
-	// useEffect(() => {
-	// 	axios
-	// 		.get(api)
-	// 		.then((res) => setPosts(res.data))
-	// 		.catch((err) => console.error(err));
-	// }, [posts]);
-
 	useEffect(() => {
 		GetPosts(dispatch);
-	}, [loading]);
+	}, [dispatch, loading]);
 
 	return (
 		<div className="feed">
