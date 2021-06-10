4490fa11617ffa4f0081f308f923fa3a424d6d18
kito0 <drewshah0@gmail.com>
2021-06-10 01:59:11 -0600

    Add message search/filter

--- client/src/components/messages/MessageSidebar/MessageSidebar.js
+++ client/src/components/messages/MessageSidebar/MessageSidebar.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import './css/message.sidebar.css';
 import MessageSidebarChat from './MessageSidebarChat';
 import { IconButton } from '@material-ui/core';
 import { DonutLarge, Chat, MoreVert, SearchOutlined } from '@material-ui/icons';
 import moment from 'moment';
+import axios from 'axios';
 
 export default function MessageSidebar({ messages, current, setCurrent }) {
+	const user = useSelector((state) => state.userSlice.user);
+	const [search, setSearch] = useState('');
+	const [filteredMessages, setFilteredMessages] = useState([]);
+
+	//const api = 'http://localhost:5000/api';
+	const api = 'https://raven-x.herokuapp.com/api';
+
+	useEffect(() => {
+		setFilteredMessages(
+			search !== ''
+				? messages.filter((message) => {
+						return message.handle1 === user.handle
+							? message.handle2.toLowerCase().includes(search.toLowerCase())
+							: message.handle1.toLowerCase().includes(search.toLowerCase());
+				  })
+				: messages
+		);
+	}, [search, user.handle, messages]);
+
+	const handleSearch = (e) => {
+		e.preventDefault();
+
+		axios.post(`${api}/conversations/create/${user.handle}/${search}`);
+	};
+
 	return (
 		<div className="message_sidebar">
 			<div className="message_sidebar_header">
@@ -21,16 +48,24 @@ export default function MessageSidebar({ messages, current, setCurrent }) {
 			</div>
 			<div className="message_sidebar_search">
 				<SearchOutlined />
-				<input placeholder="Search or start new chat" type="text" />
+				<form onSubmit={handleSearch}>
+					<input
+						placeholder="Search or start new chat"
+						type="text"
+						onChange={(e) => setSearch(e.target.value)}
+					/>
+				</form>
 			</div>
 			<div className="message_sidebar_chats">
 				{messages &&
-					messages
+					filteredMessages
 						.slice()
 						.sort(
 							(a, b) =>
+								b.messages.slice(-1)[0].timestamp &&
+								a.messages.slice(-1)[0].timestamp &&
 								moment(b.messages.slice(-1)[0].timestamp).get('millisecond') -
-								moment(a.messages.slice(-1)[0].timestamp).get('millisecond')
+									moment(a.messages.slice(-1)[0].timestamp).get('millisecond')
 						)
 						.map((conversation) => (
 							<MessageSidebarChat
