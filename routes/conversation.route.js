c7064ba90c5da96b6ade059b5bbb075888f4177c
kito0 <drewshah0@gmail.com>
2021-06-10 02:00:31 -0600

    Add createConversation endpoint

--- routes/conversation.route.js
+++ routes/conversation.route.js
@@ -3,6 +3,7 @@ const {
 	getAllConversations,
 	getConversations,
 	getConversation,
+	createConversation,
 	sendMessage,
 } = require('../controllers/conversation.controller');
 const verify = require('../util/verify.token');
@@ -11,6 +12,7 @@ router.get('/', getAllConversations);
 router.get('/:handle', getConversations);
 router.get('/:handle1/:handle2', getConversation);
 
+router.post('/create/:handle1/:handle2', createConversation);
 router.post('/:handle1/:handle2', sendMessage);
 
 module.exports = router;
