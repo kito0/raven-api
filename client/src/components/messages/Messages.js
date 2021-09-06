49774024a7472de16d27d90339559c72bb004f12
kito0 <drewshah0@gmail.com>
2021-09-06 03:04:02 -0600

    Add pusher integration with messages

--- client/src/components/messages/Messages.js
+++ client/src/components/messages/Messages.js
@@ -3,38 +3,52 @@ import { useSelector } from 'react-redux';
 import axios from 'axios';
 import MessageSidebar from './MessageSidebar';
 import MessageView from './MessageView';
-//import Pusher from 'pusher-js';
+import Pusher from 'pusher-js';
 
 export default function Messages() {
 	const user = useSelector((state) => state.userSlice.user);
 	const [messages, setMessages] = useState([]);
 	const [current, setCurrent] = useState(0);
 
-	//const api = 'http://localhost:5000';
-	const api = 'https://raven-x.herokuapp.com';
+	const api = 'http://localhost:5000';
+	//const api = 'https://raven-x.herokuapp.com';
+	//Pusher.logToConsole = true;
+
+	useEffect(
+		() => {
+			axios.get(`${api}/api/conversations/${user.handle}`).then((res) => {
+				setMessages(res.data);
+			});
+		},
+		// eslint-disable-next-line
+		[]
+		//[user.handle, messages]
+	);
+
+	// const pusher = new Pusher('9a411b2a0ee16e4825af', { cluster: 'us3' });
+	// const channel = pusher.subscribe('rvn-messenger');
+	// console.log(channel);
+	// channel.bind('my-event', (newMessage) => {
+	// 	console.log(`NEW MESSAGE: ${newMessage}`);
+	// 	setMessages([...messages, newMessage]);
+	// });
 
 	useEffect(() => {
-		axios.get(`${api}/api/conversations/${user.handle}`).then((res) => {
-			setMessages(res.data);
+		const pusher = new Pusher('9a411b2a0ee16e4825af', {
+			cluster: 'us3',
 		});
-	}, [user.handle, messages]);
 
-	// useEffect(() => {
-	// 	const pusher = new Pusher('9a411b2a0ee16e4825af', {
-	// 		cluster: 'us3',
-	// 	});
+		const channel = pusher.subscribe('messages');
+		channel.bind('my-event', (newMessage) => {
+			alert(JSON.stringify(newMessage));
+			setMessages([...messages, newMessage]);
+		});
 
-	// 	const channel = pusher.subscribe('messages');
-	// 	channel.bind('inserted', (newMessage) => {
-	// 		alert(JSON.stringify(newMessage));
-	// 		setMessages([...messages, newMessage]);
-	// 	});
-
-	// 	return () => {
-	// 		channel.unbind_all();
-	// 		channel.unsubscribe();
-	// 	};
-	// }, [messages]);
+		return () => {
+			channel.unbind_all();
+			channel.unsubscribe();
+		};
+	}, [messages]);
 
 	return (
 		<div className="messages">
--- index.js
+++ index.js
@@ -7,15 +7,18 @@ const sanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
+const Pusher = require('pusher');
 
 const userRoute = require('./routes/user.route');
 const postRoute = require('./routes/post.route');
 const conversationRoute = require('./routes/conversation.route');
 
+const pusher = new Pusher(process.env.PUSHER_CONFIG);
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const limiter = rateLimit({
-	windowMs: 60 * 1000,
+	windowMs: 1000 * 1000,
 	max: 60,
 });
 
@@ -30,6 +33,28 @@ mongoose
 		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
 	)
 	.catch((error) => console.log(error.message));
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+	const msgCollection = db.collection('Conversation');
+	console.log(msgCollection);
+	const changeStream = msgCollection.watch();
+	changeStream.on('change', (change) => {
+		console.log('change');
+		if (change.operationType === 'update') {
+			const messageDetails = change.fullDocument;
+
+			pusher.trigger('rvn-messenger', 'my-event', {
+				title: messageDetails.title,
+				handle1: messageDetails.handle1,
+				handle2: messageDetails.handle2,
+				messages: messageDetails.messages,
+				_id: messageDetails._id,
+			});
+		}
+	});
+});
 
 app.use(express.json());
 app.use(cors());
