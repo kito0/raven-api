c9ba2363a3dc889fb03899ebf2cf7f269cebe64c
kito0 <drewshah0@gmail.com>
2021-09-13 23:24:34 -0600

    Add updated conversation routes

--- routes/conversation.route.js
+++ routes/conversation.route.js
@@ -2,17 +2,17 @@ const router = require('express').Router();
 const {
 	getAllConversations,
 	getConversations,
-	getConversation,
+	// getConversation,
 	createConversation,
-	sendMessage,
+	// sendMessage,
 } = require('../controllers/conversation.controller');
 const verify = require('../util/verify.token');
 
 router.get('/', getAllConversations);
-router.get('/:handle', getConversations);
-router.get('/:handle1/:handle2', getConversation);
+router.get('/:userId', getConversations);
+// router.get('/:handle1/:handle2', getConversation);
 
-router.post('/create/:handle1/:handle2', createConversation);
-router.post('/:handle1/:handle2', sendMessage);
+router.post('/create/:firstUserId/:secondUserId', createConversation);
+// router.post('/:handle1/:handle2', sendMessage);
 
 module.exports = router;
