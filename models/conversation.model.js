7b3a8a20527aacd25a788bc68b315bc9f15337b1
kito0 <drewshah0@gmail.com>
2021-09-13 23:23:55 -0600

    Update conversation model

--- models/conversation.model.js
+++ models/conversation.model.js
@@ -2,34 +2,11 @@ const mongoose = require('mongoose');
 
 const conversationSchema = new mongoose.Schema({
 	title: {
-		type: String,
-		required: true,
+		type: String
 	},
-	handle1: {
-		type: String,
-		required: true,
-	},
-	handle2: {
-		type: String,
-		required: true,
-	},
-	messages: [
-		{
-			sender: {
-				type: String,
-				required: true,
-			},
-			text: {
-				type: String,
-				required: true,
-			},
-			timestamp: {
-				type: Date,
-				default: Date.now,
-				required: true,
-			},
-		},
-	],
-});
+	members: {
+		type: Array,
+	}
+}, { timestamps: true });
 
 module.exports = mongoose.model('Conversation', conversationSchema);
