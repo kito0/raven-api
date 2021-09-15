6e6e5b6300e74c624f78dcd1c34ee4bf3ce0b0c0
kito0 <drewshah0@gmail.com>
2021-09-14 23:37:10 -0600

    Add get last message endpoint

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -10,6 +10,15 @@ exports.getMessages = async (req, res) => {
 		.catch((err) => res.status(500).json(err));
 };
 
+// GET http://localhost:5000/api/messages/last/:conversationId
+exports.getLastMessage = async (req, res) => {
+	Message.find({ conversationId: req.params.conversationId })
+		.then((data) => {
+			res.status(200).json(data[data.length - 1]);
+		})
+		.catch((err) => res.status(500).json(err));
+};
+
 // POST http://localhost:5000/api/messages/
 exports.sendMessage = async (req, res) => {
 	Message.create(req.body)
--- routes/message.route.js
+++ routes/message.route.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const {
 	getMessages,
+	getLastMessage,
 	sendMessage,
 } = require('../controllers/message.controller');
 const verify = require('../util/verify.token');
 
 router.get('/:conversationId', getMessages);
+router.get('/last/:conversationId', getLastMessage);
 
 router.post('/', sendMessage);
 
